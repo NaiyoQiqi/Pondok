3c9a5bbb5ada40e2065d6fe4f15eebe96ab9df99
NaiyoQiqi <faifaizal933@gmail.com>
2025-02-06 20:15:02 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,36 +1,49 @@
 <?php
 
+// Set default timezone
 date_default_timezone_set('Asia/Jakarta');
-error_reporting(0);
+
+// Aktifkan error reporting untuk debugging
+error_reporting(E_ALL);  // Menampilkan semua error
+ini_set('display_errors', 1);  // Menampilkan error di halaman
+
+// Maintenance mode
 $maintenance = 0; //** 1 = ya ..  0 = tidak
-if($maintenance == 1) {
+if ($maintenance == 1) {
     die("Site under Maintenance.");
 }
-// database
+
+// Database configuration
 $config['db'] = array(
-	'host' => 'sql112.infinityfree.com',
-	'name' => 'if0_38256644_NaiyouQiqi',
-	'username' => 'if0_38256644',
-	'password' => 'NaiyouQiqi132'
+    'host' => 'sql112.infinityfree.com',  // Host Database
+    'name' => 'if0_38256644_NaiyouQiqi',  // Nama Database
+    'username' => 'if0_38256644',         // Username Database
+    'password' => 'NaiyouQiqi132'         // Password Database
 );
 
+// Cek koneksi database
 $conn = mysqli_connect($config['db']['host'], $config['db']['username'], $config['db']['password'], $config['db']['name']);
-if(!$conn) {
-	die("Koneksi Gagal : ".mysqli_connect_error());
-	}
-	
+if (!$conn) {
+    die("Koneksi Gagal : " . mysqli_connect_error());
+} else {
+    // Jika koneksi berhasil, bisa tambahkan pesan ini untuk memastikan koneksi berhasil
+    // echo "Koneksi Berhasil!";
+}
+
+// Web configuration
 $config['web'] = array(
-	'url' => 'ionsgroup.site' // isi domain anda : https://domain.com/
-	
-);	
-// date & time
+    'url' => 'ionsgroup.site'  // Isi dengan domain kamu: https://domain.com/
+);
+
+// Date and time
 $date = date("Y-m-d");
 $time = date("H:i:s");
 
-// date & time
 $tanggal = date("Y-m-d");
 $waktu = date("H:i:s");
 
+// Memanggil file lain yang diperlukan
 require("lib/function.php");
 require("lib/setting.php");
+
 ?>
